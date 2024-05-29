3fdf398cef5ca03b5038a91dbdea657c8b6b45ac
Maria <106764497+mariaeduardamoraes@users.noreply.github.com>
2024-05-29 12:18:39 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,151 +1,113 @@
-const numeroSenha = document.querySelector('.parametro-senha__texto');
-let tamanhoSenha = 12;
-numeroSenha.textContent = tamanhoSenha;
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
-const numeros = '0123456789';
-const simbolos = '!@#$%¨&*()+-';
-const campoSenha = document.querySelector('#campo-senha');
-const checkbox = document.querySelectorAll('.checkbox');
-const botoes = document.querySelectorAll('.parametro-senha__botao');
-const forcaSenha = document.querySelector('.forca');
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
-botoes[0].onclick = diminuiTamanho;
-botoes[1].onclick = aumentaTamanho;
+const perguntas = [
+    {
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: "Quis saber como usar IA no seu dia a dia."
+            }
+        ]
+    },
+    {
+        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        alternativas: [
+            {
+                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+            },
+            {
+                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+            }
+        ]
+    },
+    {
+        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+            }
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            {
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+            },
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+            }
+        ]
+    },
+    {
+        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        alternativas: [
+            {
+                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+            },
+            {
+                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+            }
+        ]
+    },
+];
 
-function diminuiTamanho() {
-    if (tamanhoSenha > 1) {
-        tamanhoSenha--;
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
     }
-    numeroSenha.textContent = tamanhoSenha;
-    geraSenha();
-
-}
-function aumentaTamanho() {
-    if (tamanhoSenha < 100) {
-        tamanhoSenha++;
-    }
-    numeroSenha.textContent = tamanhoSenha;
-    geraSenha();
-}
-
-
-
-for (i = 0; i < checkbox.length; i++) {
-    checkbox[i].onclick = geraSenha
-}
-
-geraSenha()
-
-function geraSenha() {
-    let alfabeto = '';
-    if (checkbox[0].checked) {
-        alfabeto = alfabeto + letrasMaiusculas;
-    }const numeroSenha = document.querySelector('.parametro-senha__texto');
-    let tamanhoSenha = 12;
-    numeroSenha.textContent = tamanhoSenha;
-    const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
-    const numeros = '0123456789';
-    const simbolos = '!@#$%¨&*()+-';
-    const campoSenha = document.querySelector('#campo-senha');
-    const checkbox = document.querySelectorAll('.checkbox');
-    const botoes = document.querySelectorAll('.parametro-senha__botao');
-    const forcaSenha = document.querySelector('.forca');
-    
-    botoes[0].onclick = diminuiTamanho;
-    botoes[1].onclick = aumentaTamanho;
-    
-    function diminuiTamanho() {
-        if (tamanhoSenha > 1) {
-            tamanhoSenha--;
-        }
-        numeroSenha.textContent = tamanhoSenha;
-        geraSenha();
-    
-    }
-    function aumentaTamanho() {
-        if (tamanhoSenha < 100) {
-            tamanhoSenha++;
-        }
-        numeroSenha.textContent = tamanhoSenha;
-        geraSenha();
-    }
-    
-    
-    
-    for (i = 0; i < checkbox.length; i++) {
-        checkbox[i].onclick = geraSenha
-    }
-    
-    geraSenha()
-    
-    function geraSenha() {
-        let alfabeto = '';
-        if (checkbox[0].checked) {
-            alfabeto = alfabeto + letrasMaiusculas;
-        }
-        if (checkbox[1].checked) {
-            alfabeto = alfabeto + letrasMinusculas;
-        }
-        if (checkbox[2].checked) {
-            alfabeto = alfabeto + numeros;
-        }
-        if (checkbox[3].checked) {
-            alfabeto = alfabeto + simbolos;
-        }
-    
-        let senha = '';
-    
-        for (let i = 0; i < tamanhoSenha; i++) {
-    
-            let numeroAleatorio = Math.random() * alfabeto.length;
-            numeroAleatorio = Math.floor(numeroAleatorio);
-            senha = senha + alfabeto[numeroAleatorio];
-        }
-        campoSenha.value = senha;
-        classificaSenha(alfabeto.length);
-    }
-    
-    function classificaSenha(tamanhoAlfabeto) {
-        let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
-        forcaSenha.classList.remove('fraca', 'media', 'forte');
-        if (entropia > 57) {
-            forcaSenha.classList.add('forte');
-        } else if (entropia > 35 && tamanhoSenha < 57) {
-            forcaSenha.classList.add('media');
-        } else if (entropia <= 35) {
-            forcaSenha.classList.add('fraca');
-        }
-        const valorEntropia = document.querySelector('.entropia');
-        valorEntropia.textContent ="Um computador pode levar até " + Math.floor((2 ** entropia)/(100e6*60*60*24))+" dias para descobrir essa senha";
-    }
-    if (checkbox[3].checked) {
-        alfabeto = alfabeto + simbolos;
-    }
-
-    let senha = '';
-
-    for (let i = 0; i < tamanhoSenha; i++) {
-
-        let numeroAleatorio = Math.random() * alfabeto.length;
-        numeroAleatorio = Math.floor(numeroAleatorio);
-        senha = senha + alfabeto[numeroAleatorio];
-    }
-    campoSenha.value = senha;
-    classificaSenha(alfabeto.length);
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
 }
 
-function classificaSenha(tamanhoAlfabeto) {
-    let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
-    forcaSenha.classList.remove('fraca', 'media', 'forte');
-    if (entropia > 57) {
-        forcaSenha.classList.add('forte');
-    } else if (entropia > 35 && tamanhoSenha < 57) {
-        forcaSenha.classList.add('media');
-    } else if (entropia <= 35) {
-        forcaSenha.classList.add('fraca');
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
     }
-    const valorEntropia = document.querySelector('.entropia');
-    valorEntropia.textContent ="Um computador pode levar até " + Math.floor((2 ** entropia)/(100e6*60*60*24))+" dias para descobrir essa senha";
 }
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
